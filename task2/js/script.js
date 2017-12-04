5d3e90aff85818cfac680cb3e57f877190ba579a
Alla Klimova <alla_88@inbox.lv>
2017-12-04 20:39:02 -0300

    2-nd removeImage is not depending from DOM structure

--- task2/js/script.js
+++ task2/js/script.js
@@ -34,38 +34,47 @@ function takePhoto() {
 
 function addImgToList(canvas, list) {
     let imgPath = canvas.toDataURL();
-    const removePhoto = function(e){
-        const figure = this.parentNode.parentNode;
-        list.removeChild(figure);
+    const removePhoto = function (i) {
+        return function (e) {
+
+            const figure = document.querySelector('figure[data-number="'+i+'"]');
+            list.removeChild(figure);
+        };
     };
-    const uploadImage = function(e){
-        canvas.toBlob(blob =>{
-            const formData = new FormData();
-            formData.append('image', blob);
-            const xhr = new XMLHttpRequest();
-            var boundary = String(Math.random()).slice(2);
-            xhr.open('POST', 'https://neto-api.herokuapp.com/photo-booth');
-            xhr.setRequestHeader('Content-Type', 'multipart/form-data; boundary=' + boundary);
-            xhr.addEventListener('load', () => {
-                console.log(xhr.response);
+    const uploadImage = function (i) {
+        return function (e) {
+            canvas.toBlob(blob => {
+                const formData = new FormData();
+                formData.append('image', blob);
+                //const xhr = new XMLHttpRequest();
+                // var boundary = String(Math.random()).slice(2);
+                // xhr.open('POST', 'https://neto-api.herokuapp.com/photo-booth');
+                // xhr.setRequestHeader('Content-Type', 'multipart/form-data; boundary=' + boundary);
+                // xhr.addEventListener('load', () => {
+                //     console.log(xhr.response);
+                // });
+                // xhr.send(formData);
+                fetch('https://neto-api.herokuapp.com/photo-booth', {
+                    method: 'POST',
+                    body: formData
+                })
+                    .then(r => console.log(r))
+                    .then(r => r.json())
+                    .then(data => {
+                        console.log(data)
+                    });
             });
-            xhr.send(formData);
-            // fetch('https://neto-api.herokuapp.com/photo-booth',{
-            //     method: 'POST',
-            //     body: formData
-            // })
-            //  .then(r => console.log(r));
-            // .then(r => r.json())
-            // .then(data => {
-            //     console.log(data)
-            // });
-        });
+        }
 
 
     };
     let listItem = {
         tag: 'figure',
-        attributes: [],
+        attributes: [{
+            data: {
+                number: ''
+            }
+        }],
         children: [
             {
                 tag: 'img',
@@ -79,18 +88,21 @@ function addImgToList(canvas, list) {
                     {
                         tag: 'a',
                         attributes: [{href: imgPath}, {download: 'snapshot.png'}],
-                        children: [{tag: 'i',
-                                attributes: [{
-                                    class: 'material-icons'
-                                },
-                                    {
-                                        text: 'file_download'
-                                    }]
-                        }]},
+                        children: [{
+                            tag: 'i',
+                            attributes: [{
+                                class: 'material-icons'
+                            },
+                                {
+                                    text: 'file_download'
+                                }]
+                        }]
+                    },
                     {
                         tag: 'a',
                         attributes: [],
-                        children: [{tag: 'i',
+                        children: [{
+                            tag: 'i',
                             attributes: [{
                                 class: 'material-icons'
                             },
@@ -106,7 +118,8 @@ function addImgToList(canvas, list) {
                     {
                         tag: 'a',
                         attributes: [],
-                        children: [{tag: 'i',
+                        children: [{
+                            tag: 'i',
                             attributes: [{
                                 class: 'material-icons'
                             },
@@ -120,21 +133,24 @@ function addImgToList(canvas, list) {
                         }
 
                     }
+                ]
+            }
         ]
-    }
-]
-};
+    };
     let figure = list.querySelector('figure');
-    if(figure){
-        list.insertBefore(makeItem(listItem), figure)
+    let currentNumber = list.children.length;
+    console.log('currentNumber pre', currentNumber);
+    if (figure) {
+        list.insertBefore(makeItem(listItem, currentNumber), figure)
     }
     else {
-        list.appendChild(makeItem(listItem));
+        list.appendChild(makeItem(listItem, currentNumber));
     }
 
 }
 
-function makeItem(item) {
+function makeItem(item, currentNumber) {
+
     let elem = document.createElement(item.tag);
     item.attributes.forEach(atr => {
         let keys = Object.keys(atr);
@@ -142,17 +158,21 @@ function makeItem(item) {
         if (key == 'text') {
             elem.textContent = atr[key];
         }
+        else if (key == 'data') {
+            elem.dataset.number = currentNumber;
+        }
         else {
             elem.setAttribute(key, atr[key])
         }
 
     });
-    if(item.children){
+    if (item.children) {
         item.children.forEach(child => {
-            elem.appendChild(makeItem(child))});
+            elem.appendChild(makeItem(child, currentNumber))
+        });
     }
-    if(item.action){
-        elem.addEventListener(item.action.event, item.action.function);
+    if (item.action) {
+        elem.addEventListener(item.action.event, item.action.function.call(null, currentNumber));
     }
     return elem;
 }
